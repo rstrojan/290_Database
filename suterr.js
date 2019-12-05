dad54943e176789527fb8089df8c4f14b508c025
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-05 11:19:19 -0800

    a

--- suterr.js
+++ suterr.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(session({secret:'SuperSecretPassword'}));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 34691);
+app.set('port', 34692);
 
 app.get('/',function(req,res){
 	res.render('home');
