7bd449081998bed4d6ea869b0c2a9a1444ba1031
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-05 18:21:14 -0800

    Update suterr.js

--- suterr.js
+++ suterr.js
@@ -59,15 +59,14 @@ app.get('/save',function(req,res,next){
       next(err);
       return;
     }
-	var curVals= result[0];
-	});	
-  pool.query("UPDATE workouts SET (`name`,`reps`, `weight`, `lbs`, `date`) VALUES (?,?,?,?,?)", [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.lbs || curVals.lbs, req.query.date || curVals.date], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-  });
-  
+	var curVals = result[0]
+	  pool.query("UPDATE workouts SET (`name`,`reps`, `weight`, `lbs`, `date`) VALUES (?,?,?,?,?)", [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.lbs || curVals.lbs, req.query.date || curVals.date], function(err, result){
+		if(err){
+		  next(err);
+		  return;
+		}
+	  });
+   });
   pool.query('SELECT * FROM workouts', function(err, rows, fields){
     if(err){
       next(err);
