7afe82b576eef293d4193a0c69dc37fea29a63b9
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-05 16:02:14 -0800

    Update tableEdit.js

--- public/js/tableEdit.js
+++ public/js/tableEdit.js
@@ -16,7 +16,7 @@ function deleteRow(tableID,currentRow,rowId) {
                 rowCount--;
                 i--;
 				var req = new XMLHttpRequest();
-				var payload = "{id:" + rowId + "}";
+				var payload = {id:rowId};
 				req.open('POST',"http://flip3.engr.oregonstate.edu:34692/delete", true);
 				req.setRequestHeader('Content-Type','application/json');
 				req.addEventListener('load',function(){
