943b12a0180aa98b2821d308284f0a4942cd4d7f
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-05 20:36:30 -0800

    a

--- public/js/tableEdit.js
+++ public/js/tableEdit.js
@@ -44,13 +44,13 @@ function addRow(tableID) {
         var table = document.getElementById(tableID);
 		console.log(document.getElementById("add_name").value)
 		var row = table.insertRow(2);
+		
 		row.insertCell(0).innerHTML = document.getElementById("add_name").value;
 		row.insertCell(1).innerHTML = document.getElementById("add_reps").value;
 		row.insertCell(2).innerHTML = document.getElementById("add_weight").value;
 		row.insertCell(3).innerHTML = document.getElementById("add_lbs").value;
 		row.insertCell(4).innerHTML = document.getElementById("add_date").value;
-		var but1 = row.insertCell(5);
-		document.createElement("INPUT");
+		
 		var req = new XMLHttpRequest();
 		 req.open("GET", "http://flip3.engr.oregonstate.edu:34692/insert?"+
 		 "name=" + document.getElementById("add_name").value +
