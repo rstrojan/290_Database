0a20f3a4c3dc19286ebfd879b191f6b9fe51202e
rstrojan <32496785+rstrojan@users.noreply.github.com>
2019-12-05 16:51:56 -0800

    Update tableEdit.js

--- public/js/tableEdit.js
+++ public/js/tableEdit.js
@@ -42,7 +42,7 @@ function deleteRow(tableID,currentRow,rowId) {
 function addRow(tableID) {
     try {
         var table = document.getElementById(tableID);
-		var row = table.insertRow(0);
+		var row = table.insertRow(2);
 		row.insertCell(0).innerHTML = document.getElementById(add_name);
 		row.insertCell(0).innerHTML = document.getElementById(add_reps);
 		row.insertCell(0).innerHTML = document.getElementById(add_weight);
